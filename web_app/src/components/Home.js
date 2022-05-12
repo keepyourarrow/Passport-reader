5260b74d5f0628811008a7545116245975cc1c6f
keepyourarrow <dima_p_50@mail.ru>
2022-05-12 10:19:46 +0300

    updated ngrok

--- web_app/src/components/Home.js
+++ web_app/src/components/Home.js
@@ -44,7 +44,7 @@ const Home = () => {
             }
 
             try {
-                const response = await fetch(`https://9f8e-188-243-86-226.eu.ngrok.io/${endPoint}`, obj);
+                const response = await fetch(`https://77cd-188-243-86-226.eu.ngrok.io/${endPoint}`, obj);
                 if ( response?.status > 400 ) {
                     throw new Error();
                 }
